a10d0944e1413ed07719c2ffd91cc34d04a02843
sirjofri <sirjofri@gmx.de>
2016-05-23 14:31:37 +0200

    changed font loader. hope it works EVERYWHERE now...

--- Main.java
+++ Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			Font.loadFont(getClass().getResourceAsStream("DS-DIGI.TTF"), 20);
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View.fxml"));
 			Scene scene = new Scene(root,205,295);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -21,15 +22,6 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	@Override
-	public void init() throws Exception
-	{
-		super.init();
-		
-		// @font-face and other things didn't work...
-		Font.loadFont(Main.class.getResource("./DS-DIGI.TTF").toExternalForm(), 12);
 	}
 	
 	public static void main(String[] args) {
