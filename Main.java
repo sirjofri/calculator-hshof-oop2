90bf20ca2d052521a557c45c9c15490f0f8954dc
sirjofri <sirjofri@gmx.de>
2016-05-23 14:33:07 +0200

    added comment

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Font.loadFont(getClass().getResourceAsStream("DS-DIGI.TTF"), 20);
+			Font.loadFont(getClass().getResourceAsStream("DS-DIGI.TTF"), 20); // Font not in repository. See here: http://www.dafont.com/ds-digital.font 
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View.fxml"));
 			Scene scene = new Scene(root,205,295);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
